a88ab1ba75422cd7ab8aeb374ad1ba7660c7910d
Adolfo López <indata.inteligencia@gmail.com>
2023-06-14 00:03:53 -0500

    Sincronizar con el usuario indata en Lenovo home

---
+++ funciones.js
@@ -0,0 +1,18 @@
+function doGet() {
+  
+  let html = HtmlService.createTemplateFromFile('index');
+ 
+  return html.evaluate()
+  .setTitle('Anydesk')
+  .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
+  .addMetaTag('viewport', 'width=device-width, initial-scale=1')
+  .setFaviconUrl('https://laverdadnoticias.com/__export/1513690711210/sites/laverdad/arte/apps/favicon.png_2040392579.png');
+
+}
+
+function incluirArchivo(pagina) {
+
+  let html = HtmlService.createTemplateFromFile(pagina);
+  return html.evaluate().getContent();
+  
+}
